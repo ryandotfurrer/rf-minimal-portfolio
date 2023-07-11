36b78a67408c977a8668d81351793142db81d2de
Ryan Furrer <ryanfurrer@hey.com>
2023-07-10 21:51:42 -0400

    Tryingt o get contentful to work.

--- js/contentful.js
+++ js/contentful.js
@@ -1,12 +1,12 @@
 const contentful = require('contentful');
-
 const client = contentful.createClient({
+	// This is the space ID. A space is like a project folder in Contentful terms
 	space: 'ezgyl5rfr10x',
-	environment: 'master', // defaults to 'master' if not set
-	accessToken: 'HfMne0by97WNMNLgtg6AmmC7PJbjiyRQKgHdPX2skxc',
+	// This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+	accessToken: 'CFPAT-B_rZxOj3zcghi9wfEXTTLTNe1Tsw7_vFtpDKIp1wkTo',
 });
-
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token
 client
 	.getEntry('3EUgEY91tcswVVQV3B70uI')
 	.then((entry) => console.log(entry))
-	.catch(console.error);
+	.catch((err) => console.log(err));
